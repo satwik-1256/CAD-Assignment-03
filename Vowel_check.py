37db40845f8f15d10b4d42681955738a176d9401
Satwik Srivastava <hksriv2@gmail.com>
2026-04-01 10:57:02 +0530

    all changes saved

---
+++ Vowel_check.py
@@ -0,0 +1,7 @@
+def check_vowel(ch):
+    if ch.lower() in 'aeiou':
+        print("It is a Vowel")
+    else:
+        print("It is not a Vowel")
+# taking input from user
+c = input("Enter a character: ")
